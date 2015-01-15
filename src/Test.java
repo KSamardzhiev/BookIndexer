862f1ac57a642f3b60a0c7dcb60de47c4d7d3c8c
Kostadin Samardzhiev <k.samardzhiev@gmail.com>
2015-01-15 13:42:35 +0200

    Indexer

---
+++ src/Test.java
@@ -0,0 +1,13 @@
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+
+
+public class Test {
+
+	public static void main(String[] args) throws FileNotFoundException {
+		BufferedReader in = new BufferedReader(new FileReader("test.txt"));
+
+	}
+
+}
